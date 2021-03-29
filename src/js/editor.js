35f536649a51692c222b0f3e586dcdcc924b143b
handdang <yunha97@naver.com>
2021-03-29 18:32:35 +0900

    파일추가

---
+++ src/js/editor.js
@@ -0,0 +1,7 @@
+function created(){
+    let chanel_name = document.querySelector('.sand-message-name h2').textContent;
+    let input_editor = document.querySelector('.texteditor');
+    input_editor.innerText = "message "+ chanel_name;
+    input_editor.dataset.placeholder = "message " + chanel_name;
+}
+created();
