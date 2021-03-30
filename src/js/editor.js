164e82b3e4c8ec6a1f0f0a912c47a950fd9bbc1e
handdang <yunha97@naver.com>
2021-03-30 15:47:58 +0900

    토스트추가

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,6 +1,22 @@
+const $chanelName = document.querySelector('.sand-message-name h2').textContent;
+const $texteditor = document.querySelector('.texteditor');
+const $beautyList = document.querySelectorAll('.beauty button');
+const $toast = document.querySelector('#toast');
 function created(){
-    let chanel_name = document.querySelector('.sand-message-name h2').textContent;
-    let input_editor = document.querySelector('.texteditor');
-    input_editor.dataset.placeholder = "message " + chanel_name;
+    $texteditor.dataset.placeholder = "message " + $chanelName;
 }
 created();
+function execCommand(e){
+    console.log(e);
+}
+for(let i = 0; i<$beautyList.length; i++){
+    $beautyList[i].onmouseover = function (){
+        $toast.classList = '';
+        $toast.innerText = this.className;
+        $toast.classList.add(this.className);
+    }
+    $beautyList[i].onmouseout = function (){
+        $toast.classList = '';
+    }
+}
+window.execCommand = execCommand;
