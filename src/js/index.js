ed7c06df4126f1e2f3548305d0abd56ceb1a826f
wade <wade@millie.town>
2021-04-08 17:35:43 +0900

    레이아웃 수정

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,3 @@
 import "../scss/reset.scss";
 import "./editor";
 import "./message"
-import snope from '../images/snope2.jpg';
-
-const img = document.createElement('img');
-img.src = snope;
-document.body.appendChild(img)
